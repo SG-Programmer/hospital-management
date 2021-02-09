05aab40f3e7ad09bd3ce0599c3210e58c0246d25
smit <skpatel4500@gmail.com>
2021-02-09 17:27:05 +0530

    navbar add and some implematations

--- lib/patient/PatientHomePage.dart
+++ lib/patient/PatientHomePage.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:hospital_management/patient/PatientChat.dart';
 import 'package:hospital_management/patient/PatientDoctor.dart';
-import 'package:hospital_management/utils/size.dart';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 
 class PatientHomePage extends StatefulWidget {
   @override
@@ -17,27 +17,36 @@ class _PatientHomePageState extends State<PatientHomePage>
     super.initState();
   }
 
+  GlobalKey _bottomNavigationKey = GlobalKey();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: TabBarView(
-          controller: controller,
-          children: [PatientChat(), PatientDoctor()],
-        ),
-        bottomNavigationBar: TabBar(
-          controller: controller,
-          tabs: [
-            Icon(
-              Icons.message,
-              color: Colors.blue,
-              size: screenHeight * 0.05,
-            ),
-            Icon(
-              Icons.local_hospital_rounded,
-              color: Colors.blue,
-              size: screenHeight * 0.05,
-            )
-          ],
-        ));
+      body: TabBarView(
+        controller: controller,
+        children: [PatientChat(), PatientDoctor()],
+      ),
+      bottomNavigationBar: CurvedNavigationBar(
+        key: _bottomNavigationKey,
+        index: controller.index,
+        height: 50.0,
+        items: <Widget>[
+          Icon(Icons.add, size: 30),
+          Icon(Icons.perm_identity, size: 30),
+        ],
+        color: Colors.white,
+        buttonBackgroundColor: Colors.white,
+        backgroundColor: Colors.blueAccent,
+        animationCurve: Curves.easeInToLinear,
+        animationDuration: Duration(milliseconds: 400),
+        onTap: (index) {
+          print(index);
+          setState(() {
+            controller.index = index;
+          });
+        },
+        letIndexChange: (index) => true,
+      ),
+    );
   }
 }
